6bb69a0cdab5412dbf2e13b2c8bc94c4e7995be7
samuelepietrasanta <samuele.pietrasanta@gmail.com>
2021-11-16 18:06:25 +0100

    modified homepage

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container">
-    <h1>Pagina iniziale</h1>
+    <div class="row mb-4">
+        <div class="col-12 text-center">
+            <h1>Benvenuto!</h1>
+            <h2>Qui sotto trovi il link per visualizzare tutti i nostri post</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            <a class="btn btn-primary" href="{{route('admin.posts.index')}}">VAI AI POST</a>
+        </div>
+    </div>
+    
 </div>
 @endsection
